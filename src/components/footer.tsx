8f1647cb472cfda8f52ad0624944e8380522be6a
rkeerthi <sunnykeerthi@gmail.com>
2022-10-25 21:43:40 +0530

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,13 +7,13 @@ const Footer = () => {
         loading="lazy"
         src="https://i.imgur.com/BTfF9uC.png"
         alt=""
-        className="hidden md:block"
+        className="hidden  md:block max-w-full	w-full"
       />
       <img
         loading="lazy"
         src="https://i.imgur.com/GsYAqy3.png"
         alt="block md:hidden"
-        className="block md:hidden"
+        className="block md:hidden max-w-full	w-full"
       />
     </footer>
   );
