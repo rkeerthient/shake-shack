b2367297e26e03f0fb9aeeed5a2cb82e9433e17e
rkeerthi <sunnykeerthi@gmail.com>
2022-10-25 17:00:07 +0530

    fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -42,81 +42,81 @@ const Header = () => {
         "_blank"
       );
   };
-  const [queryPrompts, setQueryPrompts] = useState<string[]>([]);
-  const words = ["CSS3.", "HTML5.", "javascript."];
-  let i = 0;
-  let timer;
+  // const [queryPrompts, setQueryPrompts] = useState<string[]>([]);
+  // const words = ["CSS3.", "HTML5.", "javascript."];
+  // let i = 0;
+  // let timer;
 
-  function typingEffect() {
-    let word = queryPrompts[i].split("");
-    var loopTyping = function () {
-      if (word.length > 0) {
-        let ele = document.querySelector(".demo") as HTMLInputElement;
-        ele.placeholder += word.shift();
-      } else {
-        deletingEffect();
-        return false;
-      }
-      timer = setTimeout(loopTyping, 100);
-    };
-    loopTyping();
-  }
+  // function typingEffect() {
+  //   let word = queryPrompts[i].split("");
+  //   var loopTyping = function () {
+  //     if (word.length > 0) {
+  //       let ele = document.querySelector(".demo") as HTMLInputElement;
+  //       ele.placeholder += word.shift();
+  //     } else {
+  //       deletingEffect();
+  //       return false;
+  //     }
+  //     timer = setTimeout(loopTyping, 100);
+  //   };
+  //   loopTyping();
+  // }
 
-  function deletingEffect() {
-    let word = queryPrompts[i].split("");
-    var loopDeleting = function () {
-      if (word.length > 0) {
-        word.pop();
-        let ele = document.querySelector(".demo") as HTMLInputElement;
-        ele.placeholder = word.join("");
-      } else {
-        if (words.length > i + 1) {
-          i++;
-        } else {
-          i = 0;
-        }
-        typingEffect();
-        return false;
-      }
-      timer = setTimeout(loopDeleting, 100);
-    };
-    loopDeleting();
-  }
+  // function deletingEffect() {
+  //   let word = queryPrompts[i].split("");
+  //   var loopDeleting = function () {
+  //     if (word.length > 0) {
+  //       word.pop();
+  //       let ele = document.querySelector(".demo") as HTMLInputElement;
+  //       ele.placeholder = word.join("");
+  //     } else {
+  //       if (words.length > i + 1) {
+  //         i++;
+  //       } else {
+  //         i = 0;
+  //       }
+  //       typingEffect();
+  //       return false;
+  //     }
+  //     timer = setTimeout(loopDeleting, 100);
+  //   };
+  //   loopDeleting();
+  // }
 
-  const fetchUnivPrompts = async () => {
-    const apiKey = "f89499a2f2d7268ee58e76e8cb6bcdff";
-    const experienceKey = "answers";
-    const experienceVersion = "STAGING";
-    const businessId = "2577253";
-    const locale = "en";
-    var url =
-      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete";
-    url += "?v=20190101";
-    url += "&api_key=" + apiKey;
-    url += "&sessionTrackingEnabled=false";
-    url += "&experienceKey=" + experienceKey;
-    url += "&input=";
-    url += "&version=" + experienceVersion;
-    url += "&locale=" + locale;
-    try {
-      let res = await fetch(url);
-      let body = await res.json();
-      let qs = body.response.results.map((item: any) => {
-        return item.value;
-      });
-      setQueryPrompts(qs);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const fetchUnivPrompts = async () => {
+  //   const apiKey = "f89499a2f2d7268ee58e76e8cb6bcdff";
+  //   const experienceKey = "answers";
+  //   const experienceVersion = "STAGING";
+  //   const businessId = "2577253";
+  //   const locale = "en";
+  //   var url =
+  //     "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete";
+  //   url += "?v=20190101";
+  //   url += "&api_key=" + apiKey;
+  //   url += "&sessionTrackingEnabled=false";
+  //   url += "&experienceKey=" + experienceKey;
+  //   url += "&input=";
+  //   url += "&version=" + experienceVersion;
+  //   url += "&locale=" + locale;
+  //   try {
+  //     let res = await fetch(url);
+  //     let body = await res.json();
+  //     let qs = body.response.results.map((item: any) => {
+  //       return item.value;
+  //     });
+  //     setQueryPrompts(qs);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchUnivPrompts();
-  }, []);
+  // useEffect(() => {
+  //   fetchUnivPrompts();
+  // }, []);
 
-  useEffect(() => {
-    queryPrompts.length >= 1 && typingEffect();
-  }, [queryPrompts]);
+  // useEffect(() => {
+  //   queryPrompts.length >= 1 && typingEffect();
+  // }, [queryPrompts]);
 
   return (
     <>
